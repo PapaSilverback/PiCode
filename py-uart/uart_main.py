0a1e6563519448873cdb40f8b79ae5558501a0e7
William Black <will.black1992@gmail.com>
2016-10-31 02:53:44 +0000

    Needed uart connection. Just some skeleton code.

---
+++ py-uart/uart_main.py
@@ -0,0 +1,10 @@
+#import uart_main.py
+from uart_ready import setup, read
+from uart_write import write
+import time
+def main():
+
+    if __name__ == "__main__": main()
+
+
+
--- py-uart/uart_write.py
+++ py-uart/uart_write.py
@@ -1,18 +1,18 @@
 
 import time
 import serial
+def write():
+    ser = serial.Serial(
+        port='/dev/ttyAMA0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+        )
+    counter = 0
 
-ser = serial.Serial(
-    port='/dev/ttyAMA0',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-    )
-counter=0
-
-while 1:
-    ser.write('Write counter: %d \n'%(counter))
-    time.sleep(1)
-    counter+=1
+    while 1:
+        ser.write('Write counter: %d \n'%(counter))
+        time.sleep(1)
+        counter += 1
